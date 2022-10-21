b8138dfd7f13c3b9b23fe8c963010c3af99bd900
MahinMolla <jobayermahin16@gmail.com>
2022-10-22 01:16:25 +0600

    change code to comment

--- problem1.py
+++ problem1.py
@@ -1,20 +1,20 @@
 
 
 
-def hello_name(name):
-   return name*2
-s=hello_name(input("Give me something: "))
-print(s)
+# def hello_name(name):
+#    return name*2
+# s=hello_name(input("Give me something: "))
+# print(s)
 
-def sleeping(weey,vacon):
-    if weey== False or vacon== True:
-        return True
-    else:
-        return  False
+# def sleeping(weey,vacon):
+#     if weey== False or vacon== True:
+#         return True
+#     else:
+#         return  False
 
-weey=input("")
-vacon=input("")
-print(sleeping(weey,vacon))
+# weey=input("")
+# vacon=input("")
+# print(sleeping(weey,vacon))
 
 a=[9,0,2,34,5,4]
 print(a[-3])
